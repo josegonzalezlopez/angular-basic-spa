cd0287965054d82f1084e3ad717cb3b2bf0058d6
Jose Maria Gonzalez Lopez <josegonzalezlopez@gmail.com>
2023-05-13 00:34:40 -0300

    agrego implementación de modulo countries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -11,8 +12,9 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
